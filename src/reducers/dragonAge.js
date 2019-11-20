7d5ac44a923db03ce7f58d89fb4a8d09bc209cec
Jose <j.a.ojeda@live.com>
2019-11-20 11:28:18 -0800

    corrected reducer to take an object

--- src/reducers/dragonAge.js
+++ src/reducers/dragonAge.js
@@ -1,17 +1,20 @@
 import { FETCH_CHARACTERS, FETCH_CHARACTERS_LOADING, FETCH_CHARACTERS_DONE } from '../actions/dragonAge';
 
 const initialState = {
-  name: '',
-  race: '',
-  img: '',
-  appearances: '',
+  character: {
+    name: '',
+    race: '',
+    img: '',
+    appearances: ''
+
+  },
   loading: true
 };
 
 export default function reducer(state = initialState, action) {
   switch(action.type) {
     case FETCH_CHARACTERS:
-      return { ...state, ...action.payload };
+      return { ...state, character: { ...action.payload } };
     case FETCH_CHARACTERS_LOADING:
       return { ...state, loading: true };
     case FETCH_CHARACTERS_DONE:
